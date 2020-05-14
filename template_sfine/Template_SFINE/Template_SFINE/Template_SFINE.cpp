85be3f78b4a3360a522d38ee5bc918ceddae6aba
Naveen Kummar <r.naveenkummar@gmail.com>
2020-05-14 14:00:01 +0530

    Template - Parameter Polymorphisim
    Template specilization using SIFNE

--- template_sfine/Template_SFINE/Template_SFINE/Template_SFINE.cpp
+++ template_sfine/Template_SFINE/Template_SFINE/Template_SFINE.cpp
@@ -2,35 +2,31 @@
 //
 
 #include <cmath>
-#include <type_traits>
 using namespace std;
 #pragma optimize("", off)
 
-//tag-dispatch
+//SIFNE - Below 'enable_if_t<is_floating_point<T>::value>' act as a class type 
+//of 'void' if T is floating type otherwise no thirsd parameter present.
 template<typename T>
-bool Equals(T lhs, T rhs, true_type) //floating
+bool Equals(T lhs, T rhs, enable_if_t<is_floating_point<T>::value>* = nullptr) //floating
 {
     return true;
 }
 
 template<typename T>
-bool Equals(T lhs, T rhs, false_type) //non-floating
+bool Equals(T lhs, T rhs, enable_if_t<!is_floating_point<T>::value>* = nullptr) //non-floating
 {
     return lhs == rhs;
 }
 
-template<typename T>
-bool Equals(T lhs, T rhs)
-{
-    return Equals(lhs, rhs, conditional_t<is_floating_point<T>::value, true_type, false_type>{});
-}
 
 
 
 
 int main()
 {
-    Equals(1, 1);
+    int* ptr = nullptr;
+    Equals(1, 1, nullptr);
     Equals(1.f, 1.f);
     Equals(1., 1.);
     Equals(1u, 1u);
