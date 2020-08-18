646bfbb5c83375333a772f6941fefa62765b4677
Naveen Kummar <naveen.kummar@kpit.com>
2020-08-18 16:49:29 +0530

    use std::forward to call copy or move ctor based on the L-Value or R-Value passed in

--- template_workout/template_umar_lone/template_umar_lone/3_perfect_forwarding.cpp
+++ template_workout/template_umar_lone/template_umar_lone/3_perfect_forwarding.cpp
@@ -69,11 +69,71 @@ public:
 		std::cout << "Employee(const std::string &&name, const Integer &&id)" << std::endl;
 	}
 
+	//step-5 - Crazy solutioon
+/*	Employee(const std::string& name, Integer&& id) :
+		m_Name{ name },
+		m_Id{ std::move(id) } {
+		std::cout << "Employee(const std::string &name, const Integer &&id)" << std::endl;
+	}*/
+
+	//Step-5a you can use std::formward to call move ctor as below
+/*	Employee(const std::string& name, Integer&& id) :
+		m_Name{ name },
+		m_Id{ std::forward<Integer&&>(id) } {
+		std::cout << "Employee(const std::string &name, const Integer &&id)" << std::endl;
+	}
+*/
+
+	//Step-5b you can use std::formward to call copy ctor as below
+	/*	Employee(const std::string& name, Integer&& id) :
+			m_Name{ name },
+			m_Id{ std::forward<Integer&>(id) } {
+			std::cout << "Employee(const std::string &name, const Integer &&id)" << std::endl;
+		}
+	*/
+
+	//Step-6 Lets templatize the ctor to avoid creation of multiple ctors as above
+/*	template<typename T1, typename T2>
+	Employee(T1&& name, T2&& id) :
+		m_Name{ name },
+		m_Id{ id } {
+		std::cout << "Template ctor Employee(std::string &&name,  Integer &&id)" << std::endl;
+
+	}
+*/
+	//Step-7 So fix problem in step-6 lets use std::forward
+	template<typename T1, typename T2>
+	Employee(T1&& name, T2&& id) :
+		m_Name{ std::forward<T1>(name) },
+		m_Id{ std::forward<T2>(id) } {
+		std::cout << "Template ctor Employee(std::string &&name,  Integer &&id)" << std::endl;
+
+	}
+
+
 };
 
 int main()
 {
-	Employee emp1{ "Naveen", 100 }; //Calls copy ctor of Interger
+	Employee emp1{ "Naveen", 100 }; //Calls copy ctor of Interger by but with step-3 move ctor will be called
+
+	//Step-4
+	const std::string str = "Naveen";
+	Employee emp2{ str, 100 }; //But this will call default ctor of Employee and copy ctor of Integer
+	//> This happened because first parameter is L-Value and second R-Value, so it called 
+	//first ctor where second R-Value is copped to id and make it as L-Value.
+
+	//Step-5
+	//> So to handle this we need to put 4 ctors in Employee class
+	//(R, R), (R,L), (L,R) and (L,L). Which is crazy 
+
+	//Step-6
+	Employee emp3{ str, Integer(100) };
+	//Above code still calls copy ctor of Integer though the templatizied ctor is called
+
+	//Though in ctor signature of templatized ctor we are passing 'Integer(100)' as 
+	//R-Value reference but it is copied to m_Id via ‘id’ directly without std::move
+	//(i.e. L-Value) so Copy ctor is called.
 
 	getchar();
 	return 0;
